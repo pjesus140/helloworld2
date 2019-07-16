958cd93699511ac404c3e43c39697309b36a6327
pjesus140 <pierre.jesus@academytrainee.com>
2019-07-16 10:52:05 +0100

    first

---
+++ src/HW/App.java
@@ -0,0 +1,15 @@
+package HW;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hello world to the standard out");
+		
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
